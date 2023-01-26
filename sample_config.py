a695a5c79c3b6da43e78a734d46bd6ffbb8ed22b
Vedanthpoojari <97805128+Vedanthpoojari@users.noreply.github.com>
2023-01-26 17:37:11 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,13 +12,13 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1997614911:AAGKVn8tLE4qRLDZiPIMo07TRy0VNEeNsrE")
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", "5938966"))
 
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "452a743cfbae846f25f48fb7d5ecdef4")
 
     # Generate a user session string
     TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
